3a8dce7c14fc554e01600cf331e7b078bca0bbc0
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-01-27 20:28:18 -1000

    finished depth first

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -4,9 +4,39 @@ const Node = require('./graphGenerator');
 
 const DFS = (start, searchFor) => {
 
-console.log('start: ', start);
-console.log('searchFor: ', searchFor);
+  let currentNode = start;
 
+  let stack = [];
+
+  let visited = [];
+
+  stack.push(currentNode);
+
+  while(stack.length !== 0) {
+    currentNode = stack.pop();
+
+    if(currentNode.value === searchFor) {
+      return currentNode;
+    }
+
+    if(visited.indexOf(currentNode) === -1) {
+      visited.push(currentNode);
+
+      currentNode.neighbors.forEach((neighbor) => {
+        stack.push(neighbor);
+        console.log('stack: ', stack);
+      });
+    }
+  }
+  return false;
 }
+
+
+
+
+
+
+
+
 
 module.exports = DFS;
